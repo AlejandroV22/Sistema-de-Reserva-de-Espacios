ab59a87eb44e1ad2a10fd23d27c31bb25d3c936e
AlejandroV22 <alejandrovelandiagelvez@gmail.com>
2025-05-24 21:18:25 -0500

    Añadido página de administrador con botones a lista de usuarios y lista de espacios

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,13 +1,15 @@
+# sistema/urls.py
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'), 
     path('panel-usuario/', views.panel_usuario_view, name='panel_usuario'),
-    path('panel/espacios/', views.panel_espacios, name='panel_espacios'),
-    path('panel/espacios/agregar/', views.agregar_espacio, name='agregar_espacio'),
-    path('admin/espacios/', views.panel_espacios, name='admin_panel_espacios'),
-    path('admin/espacios/agregar/', views.agregar_espacio, name='agregar_espacio'),
-    path('admin/espacios/eliminar/<int:espacio_id>/', views.eliminar_espacio, name='eliminar_espacio'),
-    
+
+    # ADMINISTRADOR PERSONALIZADO
+    path('admin-panel/', views.panel_administrador, name='panel_administrador'),
+    path('admin-panel/espacios/', views.panel_espacios, name='admin_panel_espacios'),
+    path('admin-panel/espacios/agregar/', views.agregar_espacio, name='agregar_espacio'),
+    path('admin-panel/espacios/eliminar/<int:espacio_id>/', views.eliminar_espacio, name='eliminar_espacio'),
+    path('admin-panel/usuarios/', views.admin_panel_usuarios, name='admin_panel_usuarios'),
 ]
--- sistema/views.py
+++ sistema/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Espacio
 from .forms import EspacioForm 
-
+from .models import Usuario
 # Create your views here.
 
 
@@ -30,3 +30,13 @@ def eliminar_espacio(request, espacio_id):
     espacio = get_object_or_404(Espacio, id=espacio_id)
     espacio.delete()
     return ('panel_espacios')
+
+def panel_administrador(request): #Panel de administrador personalizado
+    return render(request, 'panel_administrador.html')
+
+def panel_usuarios(request): #Administracion de usuarios 
+    return render(request, 'panel_usuarios.html')  
+
+def admin_panel_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'admin_panel_usuarios.html', {'usuarios': usuarios})
