7f621af89b0155ed475778736cd1b257f83a7187
Aljrf06 <138081741+Aljrf06@users.noreply.github.com>
2025-05-24 21:51:03 -0500

    commit

--- sistema/forms.py
+++ sistema/forms.py
@@ -1,7 +1,29 @@
 from django import forms
-from .models import Espacio
+from .models import Espacio, Reserva
 
 class EspacioForm(forms.ModelForm):
     class Meta:
         model = Espacio
         fields = ['nombre', 'tipoEspacio', 'capacidadMaxima']
+
+class ReservaForm(forms.ModelForm):
+   
+    fecha_Reserva = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        label="Fecha de Reserva" 
+    )
+    horaInicio = forms.TimeField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+        label="Hora de Inicio"
+    )
+    horaFin = forms.TimeField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+        label="Hora de Fin"
+    )
+
+    class Meta:
+        model = Reserva
+        fields = ['fecha_Reserva', 'horaInicio', 'horaFin']
+
+
+
--- sistema/views.py
+++ sistema/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Espacio
+from .models import Espacio, Usuario, Reserva , HorarioDisponible
 from .forms import EspacioForm 
-from .models import Usuario
+from django.http import JsonResponse
+from datetime import datetime
+
 # Create your views here.
 
 
@@ -40,3 +42,35 @@ def panel_usuarios(request): #Administracion de usuarios
 def admin_panel_usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'admin_panel_usuarios.html', {'usuarios': usuarios})
+
+def obtener_horarios_disponibles(request):
+    espacio_id = request.GET.get('espacio_id')
+    fecha_str = request.GET.get('fecha')  # ejemplo: '2025-05-24'
+
+    if not espacio_id or not fecha_str:
+        return JsonResponse({'error': 'Parámetros faltantes'}, status=400)
+
+    try:
+        fecha = datetime.strptime(fecha_str, '%Y-%m-%d').date()
+        dia_semana = fecha.weekday()  # lunes = 0, domingo = 6
+
+        horarios = HorarioDisponible.objects.filter(espacio_id=espacio_id, dia_semana=dia_semana)
+        reservas = Reserva.objects.filter(espacio_id=espacio_id, fecha_Reserva=fecha)
+
+        disponibles = []
+
+        for horario in horarios:
+            conflicto = reservas.filter(
+                horaInicio=horario.hora_inicio,
+                horaFin=horario.hora_fin
+            ).exists()
+            if not conflicto:
+                disponibles.append({
+                    'hora_inicio': horario.hora_inicio.strftime('%H:%M'),
+                    'hora_fin': horario.hora_fin.strftime('%H:%M')
+                })
+
+        return JsonResponse({'horarios': disponibles})
+
+    except ValueError:
+        return JsonResponse({'error': 'Formato de fecha inválido'}, status=400)
